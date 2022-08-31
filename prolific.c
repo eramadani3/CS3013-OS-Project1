3f61bb703db152d8d1d8ef412f245809a349117b
Engjell Ramadani <75699521+eramadani3@users.noreply.github.com>
2022-08-30 21:48:16 -0400

    Add files via upload

--- prolific.c
+++ prolific.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
     printf("hello world (pid:%d) \n", (int)getpid());
-    int rc = fork();
-    if (rc < 0)
+    int range = 11 + 1 - 5;
+    int children = 5 + (rand() % range);
+    printf("%d \n", children);
+    for (int i = 0; i < children; i++)
     {
-        fprintf(stderr, "fork failed \n");
-        exit(1)
+        if (fork() == 0)
+        {
+            printf("hello, I am child (pid:%d)\n", (int)getpid());
+            exit(0);
+        }
     }
-    else if (rc == 0)
+    for (int i = 0; i < children; i++)
     {
-        printf("hello, I am child (pid:%d)\n",(int) getpid());
+        wait(NULL);
     }
+    return 0;
 }
