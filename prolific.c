0b385a76d72c7f3072132cd4f1baae06683c3872
Engjell Ramadani <75699521+eramadani3@users.noreply.github.com>
2022-08-31 22:35:03 -0400

    Add files via upload

--- prolific.c
+++ prolific.c
@@ -7,7 +7,6 @@
 
 int main(int argc, char *argv[])
 {
-    printf("hello world (pid:%d) \n", (int)getpid());
     char *filename = "seed.txt";
     int number;
     FILE *fp = fopen(filename, "r");
@@ -21,13 +20,18 @@ int main(int argc, char *argv[])
     fclose(fp);
     srand(number * getpid());
     int children = (rand() % (11 - 5 + 1)) + 5;
-    printf("%d \n", children);
+    printf("Random seed value (converted to integer): %d \n", number);
+    printf("Random child count: %d \n", children);
+    printf("Im feeling prolific! \n");
     for (int i = 0; i < children; i++)
     {
         if (fork() == 0)
         {
-            printf("hello, I am child (pid:%d)\n", (int)getpid());
-            exit(1);
+            printf("[Parent]: I am waiting for PID %d to finish \n", (int)getpid());
+            printf("\t [Child, PID: %d]: I am the child\n", (int)getpid());
+            printf("\t [Child, PID: %d]: Now exiting... \n", (int)getpid());
+            printf("[Parent]: Child %d finished, onwards! \n", (int)getpid());
+            exit(0);
         }
     }
     for (int i = 0; i < children; i++)
