e93c1fafc43f7337aea29a0eb155192b09754170
Engjell Ramadani <eramadani@wpi.edu>
2022-09-02 13:23:20 -0400

    Fixed prolific

--- prolific.c
+++ prolific.c
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
 {
     char *filename = "seed.txt";
     int number;
-    int pids[11];
     FILE *fp = fopen(filename, "r");
     if (fp == NULL)
     {
@@ -21,20 +20,23 @@ int main(int argc, char *argv[])
     fclose(fp);
     srand(number * getpid());
     int children = (rand() % (11 - 5 + 1)) + 5;
+    int* pids = (int*) malloc(children * sizeof(int));
     printf("Random seed value (converted to integer): %d \n", number);
     printf("Random child count: %d \n", children);
     printf("Im feeling prolific! \n");
+    for (int i =0; i < children; i++){
+        pids[i] = rand();
+    }
     for (int i = 0; i < children; i++)
     {
         if (fork() == 0)
         {
-            int exitNum = ((children % 50) + 1);
-            int exitTime = ((children % 3) + 1);
-            pids[i] = (int)getpid();
-            printf("[Parent]: I am waiting for PID %d to finish \n", pids[i]);
-            printf("\t [Child, PID: %d]: I am the child and I will wait %d seconds and exit with code %d\n", pids[i], exitTime, exitNum);
-            printf("\t [Child, PID: %d]: Now exiting... \n", pids[i]);
-            printf("[Parent]: Child %d finished with status code %d. Onward! \n", pids[i], exitNum);
+            int exitNum = ((pids[i] % 50) + 1);
+            int exitTime = ((pids[i] % 3) + 1);
+            printf("[Parent]: I am waiting for PID %d to finish \n", getpid());
+            printf("\t [Child, PID: %d]: I am the child and I will wait %d seconds and exit with code %d\n", getpid(), exitTime, exitNum);
+            printf("\t [Child, PID: %d]: Now exiting... \n", getpid());
+            printf("[Parent]: Child %d finished with status code %d. Onward! \n", getpid(), exitNum);
             exit(0);
         }
     }
