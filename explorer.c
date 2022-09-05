36132041816f1b8aeb70ed7b253e71486e19585e
Engjell Ramadani <eramadani@wpi.edu>
2022-09-05 23:45:53 +0000

    Almost done

--- explorer.c
+++ explorer.c
@@ -18,44 +18,31 @@ int main(int argc, char *argv[])
     fscanf(fp, "%d", &number);
     fclose(fp);
     srand(number * getpid());
-
+    char* directories[6] = {"/home", "/proc", "proc/sys", "/usr", "/boot", "/sbin"};
     char cwd[256];  
-    char* directories[6];
-    directories[0]="/home";
-    directories[1]="/proc";
-    directories[2]="proc/sys";
-    directories[3]="/usr";
-    directories[4]="/boot";
-    directories[5]="/sbin";
+    
     
 
     printf("Random seed value (converted to integer): %d \n", number);
     printf("It's time to see the world/file system! \n");
-    for(int i = 1; i < 3; i++)
+    for(int i = 0; i < 5; i++)
     {
         int exploreNum = (rand() % 6);
         if(chdir(directories[exploreNum])!= 0) perror("chdir failed");
         if (fork() == 0)
         {
-            if (getcwd(cwd, sizeof(cwd)) == NULL) {
-                perror("getcwd() error");
-            }
-            else{
-                printf("Selection: %d \n", i);
-                printf("Current working directory is: %s\n", cwd);
-                printf("[Parent]: I am waiting for PID %d to finish \n", (int) getpid());
-                printf("\t [Child, PID: %d]: Executing 'ls -tr' command... \n",(int) getpid());
-                char* argument_list[] = {"ls", "-tr", NULL}; // NULL terminated array of char* strings
-                execvp("ls", argument_list);
-                
-            }   
+            if (getcwd(cwd, sizeof(cwd)) == NULL) perror("getcwd() error");
+            printf("Selection #%d: %s [SUCCESS]\n", i+1, directories[exploreNum]);
+            printf("Current reported directory is: %s\n", cwd);
+            printf("[Parent]: I am waiting for PID %d to finish \n", (int) getpid());
+            printf("\t [Child, PID: %d]: Executing 'ls -tr' command... \n",(int) getpid());
+            char* argument_list[] = {"ls", "-tr", NULL}; // NULL terminated array of char* strings
+            execvp("ls", argument_list);
+            exit(0);
         }
-    }
-    for (int i = 1; i < 3; i++)
-    {
-        
-        wait(NULL);
-       
+        else{
+            wait(NULL);
+        }
     }
     return 0;
 }
