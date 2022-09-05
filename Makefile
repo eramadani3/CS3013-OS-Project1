18e8adcc9983ffc3486aeacfa76a26b354733862
Engjell Ramadani <eramadani@wpi.edu>
2022-09-05 05:34:42 +0000

    Added to explorer

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
 all: prolific generation explorer
 
-prolific: 
-	gcc -Wall -o prolific prolific.c
-generation: 
-	gcc -Wall -o generation generation.c
-explorer: 
-	gcc -Wall -o explorer explorer.c
+prolific: prolific.c
+generation: generation.c
+explorer: explorer.c
--- explorer.c
+++ explorer.c
@@ -15,18 +15,35 @@ int main(int argc, char *argv[])
         printf("Error: could not open file %s", filename);
         return 1;
     }
-
     fscanf(fp, "%d", &number);
     fclose(fp);
+
+    
     srand(number * getpid());
     int exploreNum = (rand() % 6);
-    const char directories[6];
-    directories[0]="/home"
-    directories[1]="/proc"
-    directories[2]="proc/sys"
-    directories[3]="/usr"
-    directories[4]="/boot"
-    directories[5]="/sbin"
-    printf("Random Number: %d \n",exploreNum);
-    exit(0);
+    
+    char cwd[256];  
+    char* directories[6];
+    directories[0]="/home";
+    directories[1]="/proc";
+    directories[2]="proc/sys";
+    directories[3]="/usr";
+    directories[4]="/boot";
+    directories[5]="/sbin";
+
+
+    char* argument_list[] = {"ls", "-l", NULL}; // NULL terminated array of char* strings
+    printf("Random seed value (converted to integer): %d \n", number);
+    printf("Random explorer number: %d \n", exploreNum);
+    if (fork() == 0)
+    {
+        if(chdir(directories[exploreNum])!= 0) perror("chdir failed");
+        if (getcwd(cwd, sizeof(cwd)) == NULL) 
+            perror("getcwd() error");
+        else
+            printf("current working directory is: %s\n", cwd);
+            execvp("ls", argument_list);
+    }
+    wait(NULL);
+    return 0;
 }
