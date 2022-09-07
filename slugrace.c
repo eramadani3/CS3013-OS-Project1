a82f05766604126c1356905db9aac27bf122a702
Engjell Ramadani <eramadani@wpi.edu>
2022-09-07 01:12:16 +0000

    test

--- slugrace.c
+++ slugrace.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
 {
     char* filenames[4] = {"seed_slug_1.txt", "seed_slug_2.txt", "seed_slug_3.txt", "seed_slug_4.txt"};
     int* pids = (int*) malloc(4 * sizeof(int));
+    char number_buff[32];
     for (int i = 1; i <= 4; i++)
     {
         char *filename = filenames[i-1];
@@ -25,20 +26,24 @@ int main(int argc, char *argv[])
         printf("Random seed value (converted to integer): %d \n", number);
         if (fork() == 0)
         {
+            char* argument_list[2];
+            sprintf(number_buff, "%d", i);
+            argument_list[0] = number_buff;
+            argument_list[1]=NULL;
             pids[i] = (int) getpid();
-            printf("[Parent]: I am waiting for PID %d to finish \n", getpid());
-            printf("\t [Child, PID: %d]: Now exiting... \n", getpid());
+            printf("[Parent]: I forked off child %d \n", getpid());
+            printf("\t [Child, PID: %d]: Executing './slug %d' command... \n", getpid(),i);
+            execv(".",argument_list);
             exit(0);
-        }else{
-            
         }
     }
-    for (int i = 0; i < 4; i++)
-    {
-        if(wait(NULL)){
+    for(int i =0; i < 4; i++){
+        int status;
+        if(waitpid(-1,NULL,WNOHANG)){
             printf("Child finished \n");
         }else{
             usleep(20);
+            printf("The race is ongoing. The following children are still racing: \n");
         }
     }
     return 0;
